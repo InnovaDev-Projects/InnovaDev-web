a7b368158ed687e2cf88db33646f1cf17ea71b2b
Matias <mendoza.e.matias@gmail.com>
2025-09-19 15:22:36 -0300

    feat: modify astro settings for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-import vercelAdapter from '@astrojs/vercel';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
+
 export default defineConfig({
     output: 'server',
-    adapter: vercelAdapter(),
+    adapter: vercel(),
     vite: {
         plugins: [tailwindcss()],
     }
